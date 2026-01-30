46e0587d3f65082bd3f6b6b6215a86f4288df30c
LakshayAseri <lakshayaseri1@gmail.com>
2026-01-30 07:03:53 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 114
+let accountemail = "lakshay@gmail.com"
+var accountpass = "12345"
+accountcity = "jaipur"
+
+//accountId = 2
+console.log(accountId);
+accountemail="Aseri"
+accountpass=43321
+accountcity="bundi"
+let accountState;
+console.table([accountemail , accountpass , accountcity,accountState])
